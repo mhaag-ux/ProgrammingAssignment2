b7af7fffa73f1bd53f98cfdff2dbbd8bc6025029
mhaag-ux <marvinhaag@web.de>
2019-10-31 13:02:39 +0100

    just testing the git again

--- example.R
+++ example.R
@@ -1,13 +1,3 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
 makeVector <- function(x = numeric()) {
         m <- NULL
         set <- function(y) {
@@ -21,16 +11,6 @@ makeVector <- function(x = numeric()) {
              setmean = setmean,
              getmean = getmean)
 }
-
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
-
-
 
 
 cachemean <- function(x, ...) {
